7ae1abc7fd0c73a378fbbd35ac78bc3c4ef1e47d
mahmoudnabil133 <hodanabil144@gmail.com>
2024-07-23 20:53:01 +0300

    fifo_cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+"""
+implement fifo cache
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+
+    def __init__(self):
+        "init method"
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        "define put method"
+        if not key or not item:
+            return
+        if not self.cache_data.get(key):
+            if len(self.cache_data) >= FIFOCache.MAX_ITEMS:
+                removed = self.cache_data.popitem(0)
+                print('DISCARD: ', removed[0])
+        self.cache_data[key] = item
+
+    def get(self, key):
+        "(hit) get item from cache"
+        if not self.cahce_data.get(key):
+            return None
+        return self.cache_data[key]

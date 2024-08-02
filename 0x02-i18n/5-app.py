80d349b9ad7357df4ba470e919046c145befabb1
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-02 21:27:21 +0300

    user_logged_in_or_not

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+'''Task 0: Basic Flask app
+'''
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    '''Config class'''
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@app.before_request
+def before_request():
+    "make g.user has the value of current user"
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    if request.args.get('login_as'):
+        return users.get(int(request.args.get('login_as')))
+    return None
+
+
+@app.route('/')
+def index():
+    '''default route'''
+    return render_template("5-index.html", )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
